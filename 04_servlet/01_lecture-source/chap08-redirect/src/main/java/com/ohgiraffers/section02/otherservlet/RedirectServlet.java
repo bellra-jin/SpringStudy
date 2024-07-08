440b3076445b7e8b17e9c5511577064ca9ab3954
bellrajin <parkjin931@gmail.com>
2024-07-08 14:50:03 +0900

    forward&redirect 공부

---
+++ 04_servlet/01_lecture-source/chap08-redirect/src/main/java/com/ohgiraffers/section02/otherservlet/RedirectServlet.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.section02.otherservlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+// forward -> get 요청은 get으로 post 요청은 포워드
+// redirect -> get, post요청 전부 get으로 redirect
+@WebServlet("/redirect")
+public class RedirectServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("이 서블릿으로 redirect 성공!");
+    }
+}
