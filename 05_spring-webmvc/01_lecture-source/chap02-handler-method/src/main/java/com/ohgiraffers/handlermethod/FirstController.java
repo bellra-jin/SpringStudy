9622fe5d0031375e5c3cf8554ddc69733fda6452
bellrajin <parkjin931@gmail.com>
2024-07-11 15:02:27 +0900

    ModelAndView 공부

--- 05_spring-webmvc/01_lecture-source/chap02-handler-method/src/main/java/com/ohgiraffers/handlermethod/FirstController.java
+++ 05_spring-webmvc/01_lecture-source/chap02-handler-method/src/main/java/com/ohgiraffers/handlermethod/FirstController.java
@@ -1,25 +1,31 @@
 package com.ohgiraffers.handlermethod;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.context.request.WebRequest;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/first/*")
+@SessionAttributes("id")
 public class FirstController {
 
     // /first/regist 요청이 들어오면
     // void 메소드인 경우 요청 주소가 곧 view의 이름이 된다.
     // return "/first/regist" 를 작성해주는 것과 같다.
     @GetMapping("regist")
-    public void regist() {}
+    public void regist() {
 
-    /** 1. WebRequest로 요청 파라미터 전달 받기
-     *
-     */
+    }
+
+    /* 1. WebRequest로 요청 파라미터 전달 받기
+     * */
     @PostMapping("regist")
     public String registMenu(Model model, WebRequest request) {
 
@@ -27,11 +33,100 @@ public class FirstController {
         int menuPrice = Integer.parseInt(request.getParameter("menuPrice"));
         int categoryCode = Integer.parseInt(request.getParameter("categoryCode"));
 
-        String message = menuName + "을(를) 신규 메뉴의 목록의 " + categoryCode + "번 카테고리에 " + menuPrice + "원으로 등록하셨습니다!";
+        String message = menuName + "을(를) 신규 메뉴 목록의 " + categoryCode + "빈 카테고리에 " + menuPrice + "원으로 등록하셨습니다!";
+        model.addAttribute("message", message);
+        return "first/messagePrinter";
+    }
+
+    @GetMapping("modify")
+    public void modify() {
+
+    }
+
+    @PostMapping("modify")
+    public String modifyenuPrice(Model model,
+                                 @RequestParam String modifyName,
+                                 @RequestParam(defaultValue = "0") int modifyPrice) {
+        System.out.println("modifyName = " + modifyName);
+        System.out.println("modifyPrice = " + modifyPrice);
+
+        String message = modifyName + " 메뉴의 가격을 " + modifyPrice + "원으로 가격을 변경하였습니다.";
 
         model.addAttribute("message", message);
-
         return "first/messagePrinter";
+    }
+
+    //Map은 지향.. 에러코드가 발생되기 어려움
+    @PostMapping("modifyAll")
+    public String modifyAll(Model model,
+                            @RequestParam Map<String, String> parameters) {
+        String modifyName = parameters.get("modifyName2");
+        int modifyPrice = Integer.parseInt(parameters.get("modifyPrice2"));
+        String message = "메뉴의 이름을 " + modifyName + "(으)로, 가격을 " + modifyPrice + "원 으로 변경하였습니다.";
+        model.addAttribute("message", message);
+        return "first/messagePrinter";
+    }
+
+    @GetMapping("search")
+    public void search() {
+
+    }
+
+    @PostMapping("search")
+    public String searchMenu(@ModelAttribute("menu") MenuDTO menu) {
+        System.out.println(menu);
+        return "first/searchResult";
+    }
+
+    @GetMapping("login")
+    public void login() {}
+
+    @PostMapping("login1")
+    public String sessionTest1(HttpSession session, @RequestParam String id) {
+
+        session.setAttribute("id", id);
+
+        return "first/loginResult";
+    }
+
+    @GetMapping("logout1")
+    public String logoutTest1(HttpSession session) {
+
+//        session.removeAttribute("id");
+//        session.setMaxInactiveInterval(600);
+        session.invalidate();
+
+        return "first/loginResult";
+    }
+
+    @PostMapping("login2")
+    public String sessionTest2(Model model, @RequestParam String id){
+
+        model.addAttribute("id", id);
+
+        return "first/loginResult";
+    }
+
+    // session에 남겨져 있는 값 제거
+    @GetMapping("logout2")
+    public String logoutTest2(SessionStatus status){
+
+        status.setComplete();
+
+        return "first/loginResult";
+    }
+
+    @GetMapping("body")
+    public void body() {}
+
+    @PostMapping("body")
+    public void bodyTest(@RequestBody String body,
+                         @RequestHeader("content-type")String contentType,
+                         @CookieValue(value = "JSESSIONID", required = false) String sessionId) throws UnsupportedEncodingException {
+
+        System.out.println(URLDecoder.decode(body, "UTF-8"));
+        System.out.println(contentType);
+        System.out.println(sessionId);
     }
 
 }
