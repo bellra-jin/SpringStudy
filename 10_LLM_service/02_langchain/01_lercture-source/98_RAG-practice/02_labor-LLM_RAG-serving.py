c697a10fad7a08da93cf8b4c207ae392ac724c3e
bellrajin <parkjin931@gmail.com>
2024-09-06 15:06:01 +0900

    파이썬 공부 5일차 - 3

--- 10_LLM_service/02_langchain/01_lercture-source/98_RAG-practice/02_labor-LLM_RAG-serving.py
+++ 10_LLM_service/02_langchain/01_lercture-source/98_RAG-practice/02_labor-LLM_RAG-serving.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chat_models import ChatOllama
+from langchain_community.chat_models import ChatOllama
 from langchain_core.runnables import RunnablePassthrough
 
 # 랭스미스 연결
@@ -21,10 +21,8 @@ vectorstore = FAISS.load_local(
 # retriever 생성
 retriever = vectorstore.as_retriever(
     search_type="similarity",
-    search_kwars={"k":5}
+    search_kwargs={"k":5}
 )
-
-# print(retriever.batch({"빵"}))
 
 # prompt 생성
 message = """
@@ -39,13 +37,12 @@ Context:
 # 프롬프트 템플릿 생성
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", "당신은 어린 아이에게 아주 친절한 유치원 선생님 입니다. 질문하는 아이의 동심이 파괴되지 않게 잘 답변을 해주세요."),
+        ("system", "당신은 근로기준법에 대해 아주 많이 알고 있습니다. 친절하게 대답해줍니다."),
         ("human", message)
     ]
 )
 
 # LLM 모델 로드
-
 model = ChatOllama(
     model = "gemma2:latest",
     temperature=0.3
@@ -54,13 +51,13 @@ model = ChatOllama(
 # RAG chain 생성
 chain = {"context": retriever, "question" : RunnablePassthrough()} | prompt | model
 
-# print(chain.invoke("백설공주의 이름이 왜 백설공주야?"))
+# print(chain.invoke("근로기준법이란 뭐야?"))
 
 from fastapi import FastAPI, Form
 from typing import Annotated
 
 app = FastAPI()
 
-@app.post("/snow-white", tags=["백설공주"])
-async def snow_white(question: Annotated[str, Form()]):
+@app.post("/Labor", tags=["근로기준법"])
+async def Labor(question: Annotated[str, Form()]):
     return {"answer": chain.invoke(question).content}
